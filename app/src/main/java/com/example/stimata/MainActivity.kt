9760f14eaa086c59831eaf879547ec59d0b34b1e
Dika <baktiarmahardika08@gmail.com>
2024-10-23 20:09:57 +0700

    intent exsplisit

---
+++ app/src/main/java/com/example/stimata/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.stimata
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
+import com.example.stimata.ui.theme.StimataTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)){v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnMoveActivity = findViewById<Button>(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener{
+            onClick()
+        }
+    }
+    private fun onClick(){
+        val intent = Intent(applicationContext, MoveActivity::class.java)
+        startActivity(intent)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StimataTheme {
+        Greeting("Androids")
+    }
+}
